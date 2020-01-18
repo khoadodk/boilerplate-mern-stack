1b0a481f462e4db3782c183af46668eead432761
Khoa Do <khoado.dk@gmail.com>
2020-01-18 17:48:59 -0500

    Added forgot/reset password API

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,17 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 //Controllers
-const { signup, accountActivation, signin } = require('../controllers/auth');
+const {
+  signup,
+  accountActivation,
+  signin,
+  forgotPassword,
+  resetPassword
+} = require('../controllers/auth');
 
 //Validators
 const {
   userSignupValidator,
-  userSigninValidator
+  userSigninValidator,
+  forgotPasswordValidator,
+  resetPasswordValidator
 } = require('../validators/auth');
 const { runValidation } = require('../validators');
 
 router.post('/signup', userSignupValidator, runValidation, signup);
 router.post('/account-activation', accountActivation);
 router.post('/signin', userSigninValidator, runValidation, signin);
+router.put(
+  '/forgot-password',
+  forgotPasswordValidator,
+  runValidation,
+  forgotPassword
+);
+router.put(
+  '/reset-password',
+  resetPasswordValidator,
+  runValidation,
+  resetPassword
+);
 
 module.exports = router;
